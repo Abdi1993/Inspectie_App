e84f625e8a4e75079435362d5ef683f9c8f98c73
Abdi Abd Alkader <1740199abdalkader@zuyd.nl>
2020-03-10 19:04:31 +0100

    Add project files.

---
+++ Inspectie_App/Inspectie_App/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Inspectie_App
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        static public HomePage home {get;set;}
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            
+            await Navigation.PushAsync(new HomePage());
+        }
+    }
+}
---
+++ Inspectie_App/Inspectie_App/ProjectenPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inspectie_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectenPage : ContentPage
+    {
+        public ProjectenPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectsEditBtn_Clicked(object sender, EventArgs e)
+        {
+            ToolbarItem item = (ToolbarItem)sender;
+            Navigation.PushAsync(new AddProjectenPage());
+        }
+    }
+}
